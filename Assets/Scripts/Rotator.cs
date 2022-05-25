d1f593c91d347aface4aca2d1a59d90c74424cf8
MuratYetisen <murat.ytsn@gmail.com>
2022-05-25 14:14:03 +0300

    25-05-22-Fixes

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Rotator : MonoBehaviour
 {
-    public float speed = 100f;
+    [SerializeField] float _speed = 150f;
+    [SerializeField] Text _scoreText;
+    int _score=4,_level=4;
+    private void Awake()
+    {
+        _scoreText.text = _score.ToString();
+    }
+    private void FixedUpdate()
+    {
+        
+        transform.Rotate(0f, 0f, _speed * Time.deltaTime);
+        LevelSystem();
+        _scoreText.text = _score.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pin")
+        {
+            Debug.Log("carpti");
+            _score--;
+
+        }
+    }
+
     private void Update()
     {
-        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        
+    }
+    void LevelSystem()
+    {
+        if (_score==0)
+        {
+            _level += 1;
+            _score = _level;
+            //Destroy(GameObject.FindGameObjectWithTag("Pin"));
+            //Destroy(GetComponentInChildren<GameObject>());
+        }
+        
     }
 }
